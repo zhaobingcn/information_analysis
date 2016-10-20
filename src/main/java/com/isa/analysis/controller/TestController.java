37898e8d9a707acc7e7aeb468bcb25be56147c55
zhaobingcn <zhaobingcn@hotmail.com>
2016-10-20 21:53:21 +0800

    add a todo tag

--- src/main/java/com/isa/analysis/controller/TestController.java
+++ src/main/java/com/isa/analysis/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by hexu on 2016/10/19.
  */
 @RestController
-public class TestController {                       // TODO: 2016/10/20 need to completed
+public class TestController {                       // TODO: 2016/10/20 need to be completed
     
 }
