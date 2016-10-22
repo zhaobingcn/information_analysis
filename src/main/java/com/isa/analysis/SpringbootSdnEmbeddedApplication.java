aef8f9095c0b8014f23646a9097f9219b3393b29
zhaobingcn <zhaobingcn@hotmail.com>
2016-10-22 22:27:49 +0800

    modified the springbootsdnembeddedapplication

--- src/main/java/com/isa/analysis/SpringbootSdnEmbeddedApplication.java
+++ src/main/java/com/isa/analysis/SpringbootSdnEmbeddedApplication.java
@@ -2,13 +2,14 @@ package com.isa.analysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author zhaobing
  */
 @SpringBootApplication
-
-public class SpringbootSdnEmbeddedApplication {
+public class SpringbootSdnEmbeddedApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args){
         SpringApplication.run(SpringbootSdnEmbeddedApplication.class, args);
