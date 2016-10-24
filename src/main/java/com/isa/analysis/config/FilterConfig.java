13b79d1e9a4edfbc8d6f6bff1e488d2de268bd2f
zhaobingcn <zhaobingcn@hotmail.com>
2016-10-24 14:33:19 +0800

    add filiterconfig.java

---
+++ src/main/java/com/isa/analysis/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package com.isa.analysis.config;
+
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+/**
+ * Created by zhzy on 2016/10/22.
+ */
+@Configuration
+public class FilterConfig{
+    @Bean
+    public CharacterEncodingFilter initlizeCharacterEncodingFilter(){
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
+    }
+}
