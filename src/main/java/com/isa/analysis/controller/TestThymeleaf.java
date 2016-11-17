42ad429f21d10d0850e146fcf9285527626cd903
zhaobingcn <zhaobingcn@hotmail.com>
2016-11-17 17:06:38 +0800

    11.17.17.06

---
+++ src/main/java/com/isa/analysis/controller/TestThymeleaf.java
@@ -0,0 +1,37 @@
+package com.isa.analysis.controller;
+
+/**
+ * Created by hexu on 2016/11/17.
+ */
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+/**
+
+ * 模板测试.
+
+ * @author Administrator
+
+ *
+
+ */
+
+@Controller
+
+public class TestThymeleaf {
+/**
+
+ * 返回html模板.
+
+ */
+
+@RequestMapping("/helloHtml")
+public String helloHtml(Map<String,Object> map){
+
+        map.put("hello","from TemplateController.helloHtml");
+        return "/helloHtml";
+        }
+        }
