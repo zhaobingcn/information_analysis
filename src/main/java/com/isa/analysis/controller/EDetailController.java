82e3321a968bc26d9beb23f0af25a45321bf5cc2
zhaobingcn <zhaobingcn@hotmail.com>
2017-02-24 15:40:55 +0800

    2.24.12

--- src/main/java/com/isa/analysis/controller/EDetailController.java
+++ src/main/java/com/isa/analysis/controller/EDetailController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
